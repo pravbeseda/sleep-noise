0a60ee7a8a294a42ce46cf983db0419f0253e7d3
Alexander Ivanov <pravbeseda@yandex.ru>
2025-01-01 19:05:41 +0500

    Themes

--- app/src/main/java/ru/pravbeseda/whitenoise/MainActivity.kt
+++ app/src/main/java/ru/pravbeseda/whitenoise/MainActivity.kt
@@ -2,14 +2,17 @@ package ru.pravbeseda.whitenoise
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.SeekBar
 import ru.pravbeseda.whitenoise.media.BrownNoiseGenerator
 import ru.pravbeseda.whitenoise.media.WhiteNoiseGenerator
 
+const val APP_PREFS = "AppPreferences"
 const val WHITE_NOISE_VOLUME = "whiteNoiseVolume"
 const val BROWN_NOISE_VOLUME = "brownNoiseVolume"
+const val CURRENT_THEME = "selectedTheme"
 
 class MainActivity : AppCompatActivity() {
     private val whiteNoiseGenerator = WhiteNoiseGenerator()
@@ -18,10 +21,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        preferences = getSharedPreferences(APP_PREFS, MODE_PRIVATE)
+        applyTheme(preferences.getString(CURRENT_THEME, "system") ?: "system")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        preferences = getSharedPreferences("NoisePreferences", MODE_PRIVATE)
+        val themeButton: Button = findViewById(R.id.toggleThemeButton)
+
+        themeButton.setOnClickListener {
+            toggleTheme()
+        }
 
         val playPauseButton: Button = findViewById(R.id.playPauseButton)
         val whiteNoiseVolume: SeekBar = findViewById(R.id.whiteNoiseVolume)
@@ -84,6 +94,28 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         stopNoise()
+    }
+
+    private fun toggleTheme() {
+        val currentTheme = preferences.getString(CURRENT_THEME, "system") ?: "system"
+        val newTheme = when (currentTheme) {
+            "system" -> "light"
+            "light" -> "dark"
+            "dark" -> "system"
+            else -> "system"
+        }
+
+        preferences.edit().putString(CURRENT_THEME, newTheme).apply()
+        recreate()
+    }
+
+    private fun applyTheme(theme: String) {
+        Log.d("MainActivity", "Applying theme: $theme")
+        when (theme) {
+            "system" -> setTheme(R.style.Theme_WhiteNoise_System)
+            "light" -> setTheme(R.style.Theme_WhiteNoise_Light)
+            "dark" -> setTheme(R.style.Theme_WhiteNoise_Dark)
+        }
     }
 }
 
