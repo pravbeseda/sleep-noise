31ac9a4e2adcd4ededd882ca89540ea9577edac7
Alexander Ivanov <pravbeseda@yandex.ru>
2025-01-01 13:34:20 +0500

    Autosaving volumes

--- app/src/main/java/ru/pravbeseda/whitenoise/MainActivity.kt
+++ app/src/main/java/ru/pravbeseda/whitenoise/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.pravbeseda.whitenoise
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
@@ -7,23 +8,32 @@ import android.widget.SeekBar
 import ru.pravbeseda.whitenoise.media.BrownNoiseGenerator
 import ru.pravbeseda.whitenoise.media.WhiteNoiseGenerator
 
+const val WHITE_NOISE_VOLUME = "whiteNoiseVolume"
+const val BROWN_NOISE_VOLUME = "brownNoiseVolume"
+
 class MainActivity : AppCompatActivity() {
     private val whiteNoiseGenerator = WhiteNoiseGenerator()
     private val brownNoiseGenerator = BrownNoiseGenerator()
     private var isPlaying = false
+    private lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        preferences = getSharedPreferences("NoisePreferences", MODE_PRIVATE)
+
         val playPauseButton: Button = findViewById(R.id.playPauseButton)
         val whiteNoiseVolume: SeekBar = findViewById(R.id.whiteNoiseVolume)
         val brownNoiseVolume: SeekBar = findViewById(R.id.brownNoiseVolume)
 
-        whiteNoiseVolume.progress = 50
-        brownNoiseVolume.progress = 50
-        whiteNoiseGenerator.setVolume(0.5f)
-        brownNoiseGenerator.setVolume(0.5f)
+        val whiteVolume = preferences.getFloat(WHITE_NOISE_VOLUME, 0.5f)
+        val brownVolume = preferences.getFloat(BROWN_NOISE_VOLUME, 0.5f)
+
+        whiteNoiseVolume.progress = (whiteVolume * 100).toInt()
+        brownNoiseVolume.progress = (brownVolume * 100).toInt()
+        whiteNoiseGenerator.setVolume(whiteVolume)
+        brownNoiseGenerator.setVolume(brownVolume)
 
         playPauseButton.setOnClickListener {
             if (isPlaying) {
@@ -38,7 +48,9 @@ class MainActivity : AppCompatActivity() {
 
         whiteNoiseVolume.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                whiteNoiseGenerator.setVolume(progress / 100f)
+                val volume = progress / 100f
+                whiteNoiseGenerator.setVolume(volume)
+                saveVolume(WHITE_NOISE_VOLUME, volume)
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
@@ -46,11 +58,17 @@ class MainActivity : AppCompatActivity() {
 
         brownNoiseVolume.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                brownNoiseGenerator.setVolume(progress / 100f)
+                val volume = progress / 100f
+                brownNoiseGenerator.setVolume(volume)
+                saveVolume(BROWN_NOISE_VOLUME, volume)
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
+    }
+
+    private fun saveVolume(key: String, volume: Float) {
+        preferences.edit().putFloat(key, volume).apply()
     }
 
     private fun startNoise() {
