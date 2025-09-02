64207d25d2627ebb46e483e3a34647390e8c2a64
Alexander Ivanov <pravbeseda@yandex.ru>
2025-09-02 06:46:51 +0400

    Release 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "ru.pravbeseda.sleepnoise"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
